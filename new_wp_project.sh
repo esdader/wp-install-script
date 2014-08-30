e3fcaeb74b63a801c012e2e4f589cd43fc57ef24
Tristan Ader <tristanader@gmail.com>
2014-08-30 18:15:05 -0500

    fixed bug with commands and added style auto edit

--- new_wp_project.sh
+++ new_wp_project.sh
@@ -9,10 +9,14 @@ if [ "$RESP" = "y" ]; then
     echo -e "Starting new WordPress project . . .\n"
     wp core download
     echo -e "\n\n"
+    read -p "Do you need to create a database? (y/n) " RESP
     read -p "Enter database name: " DBNAME
     read -p "Enter database user: " DBUSER
     read -p "Enter database host: " DBHOST
     read -s -p "Enter database password: " DBPASSWORD 
+    if [ "$RESP" = "y" ]; then
+        mysqladmin -u"$DBUSER" -p"$DBPASSWORD" create "$DBNAME"
+    fi
     echo -e "\n\nCreating config . . .\n"
     wp core config --dbname="$DBNAME" --dbuser="$DBUSER" --dbhost="$DBHOST" --dbpass="$DBPASSWORD"
     
@@ -27,15 +31,15 @@ if [ "$RESP" = "y" ]; then
     echo -e "\n\nStart installing plugins . . .\n"
     read -p "Install Advanced Custom Fields? (y/n) " RESP
 
-    if [ "$RESP" = "y"]; then
+    if [ "$RESP" = "y" ]; then
         echo -e "Installing Advanced Custom Fields . . .\n"
-        wp plugin install "/Users/tristanader/Dropbox/plugins/wordpress\ plugins/advanced-custom-fields-pro.zip"
+        wp plugin install "/Users/tristanader/Dropbox/plugins/wordpress plugins/advanced-custom-fields-pro.zip"
         wp plugin activate "advanced-custom-fields-pro"
     fi
 
     read -p "Install Custom Post UI? (y/n) " RESP
 
-    if [ "$RESP" = "y"]; then
+    if [ "$RESP" = "y" ]; then
         echo -e "Installing Custom Post UI. . .\n"
         wp plugin install "custom-post-type-ui"
         wp plugin activate "custom-post-type-ui"
@@ -43,22 +47,23 @@ if [ "$RESP" = "y" ]; then
 
     read -p "Install WP Migrate Pro? (y/n) " RESP
 
-    if [ "$RESP" = "y"]; then
+    if [ "$RESP" = "y" ]; then
         echo -e "Installing WP Migrate Pro. . .\n"
-        wp plugin install "/Users/tristanader/Dropbox/plugins/wordpress\ plugins/wp-migrate-db-pro.zip" 
+        wp plugin install "/Users/tristanader/Dropbox/plugins/wordpress plugins/wp-migrate-db-pro.zip" 
         wp plugin activate "wp-migrate-db-pro"
     fi
 
     echo -e "\n\nDone installing plugins. Install starter theme next\n"
     read -p "Install Starter Theme? (y/n) " RESP
 
-    if [ "$RESP" = "y"]; then
+    if [ "$RESP" = "y" ]; then
         read -p "What do you want to call this theme? " THEME_NAME
         echo -e "\nInstalling Starter theme. . .\n"
         cd "wp-content/themes"
         git clone https://github.com/esdader/petulant-octo-wight.git "$THEME_NAME"
         cd "$THEME_NAME"
         rm "./README.md"
+        sed "s/Starter\ Theme/$THEME_NAME/" style.css > tmp.css ; mv tmp.css style.css
         rm -rf "./git"
         git init
         git add .
